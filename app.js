47d11cc3b3bebcb718c6b8492459337abbed32be
Erwin <ejhessing@gmail.com>
2016-07-04 12:50:18 +1200

    Basic Template of calculator

---
+++ app.js
@@ -0,0 +1,7 @@
+// Need to get all the html elements
+// Create a currentEquation and currentTotal that will display
+// As buttons pushed, it will add to the current equation
+// The display can only show so many numbers
+// Current Equation could be an array that keeps track of everything pushed
+
+
