2710767aa5a95f89e488f55a7b3c86b9aa449bce
Erwin <ejhessing@gmail.com>
2016-07-06 15:30:33 +1200

    Added comments for refactored functions

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 //Varibles
-var equation = [];
-var x, y;
-var currentAnswer = null;
-var temp = '';
-var currentOperator ;
-var sign = 1;
+var x, y,
+    sign = 1,
+    temp = '',
+    equation = [],
+    currentAnswer = null;
+
 
 startCalculator();
 
@@ -23,51 +23,64 @@ function startCalculator () {
 
 
 function capture (evt) {
-  var clicked = evt.target.innerHTML;
-  var symbol = /[+\-X%/]/
+  var clicked = evt.target.innerHTML,
+      symbol = /[+\-X%/]/;
 
-  if(clicked === "AC"){
-    reset();
-  } else if(clicked === "CE"){
-    temp = temp.substring(0,temp.length - 1);
-    display(temp);
-  } else if(clicked === "=") {
-    addToEquation(temp * sign);
-    calculate();
-  } else if(clicked === "+/-"){
-      sign *= -1;
-  } else if(symbol.test(clicked)){
+
+  switch(clicked) {
+    case "AC":
+      reset();
+      break;
+    case "CE":
+      temp = temp.substring(0,temp.length - 1);
+      display(temp);
+      break;
+    case "=":
       addToEquation(temp * sign);
-      addToEquation(clicked);
-      temp = '';
-      display(clicked);
-  } else {
-    temp += evt.target.innerHTML;
-    display(temp);
+      calculate();
+      break;
+    case "+/-":
+      sign *= -1;
+      break;
+    default:
+    //if clicked is a symbol, then run addSymbol function or add number to temp
+    if(symbol.test(clicked)){
+       addSymbol(clicked);
+    } else {
+      temp += evt.target.innerHTML;
+      display(temp);
+    }
   }
-
 }
 
+//add the number in temp into array, the
+function addSymbol (symbol) {
+  addToEquation(temp * sign);
+  addToEquation(symbol);
+  temp = '';
+  display(symbol);
+}
 
-
+//displays text
 function display (text) {
   var display = document.getElementById('text');
   display.innerHTML = text;
 }
 
-
+//add to the equation array and change the sign back to positive
 function addToEquation (item) {
   equation.push(item);
   sign = 1;
 }
 
-
+//Runs when the equal button is pressed
 function calculate () {
-  for(var i =0; i<equation.length; i++) {
-    if(!isNaN(equation[i])) {
-      if(x === null || x === undefined) {
+  for (var i =0; i<equation.length; i++) {
+    //if is a number then change x or y depending if they are already defined
+    if (!isNaN(equation[i])) {
+      if (x === null || x === undefined) {
         x = equation[i];
-      } else if(currentAnswer !== null) {
+      } else if (currentAnswer !== null) {
         x = currentAnswer;
         y = equation[i];
       } else {
@@ -77,40 +90,15 @@ function calculate () {
 
    currentAnswer = runLogic(equation[i], Number(x), Number(equation[i+1]));
 
-    // switch (equation[i]) {
-    //   case "+":
-    //     currentAnswer = Number(x) + Number(equation[i+1]);
-    //     break;
-    //   case "-":
-    //     currentAnswer = Number(x) - Number(equation[i+1]);
-    //     break;
-    //   case "X":
-    //     currentAnswer = Number(x) * Number(equation[i+1]);
-    //     break;
-    //   case "/":
-    //     currentAnswer = Number(x) / Number(equation[i+1]);
-    //     break;
-    //   case "%":
-    //     currentAnswer = Number(x) % Number(equation[i+1]);
-    //     break;
-    // }
-
   }
-  console.log(currentAnswer)
+
   display(currentAnswer);
-  equation = ["currentAnswer"];
-}
-
-function reset() {
-      equation = [];
-      temp = '';
-      x = null;
-      y = null;
-      display("");
-      currentAnswer = null;
+  equation = [currentAnswer];
 }
 
 
+
+//Calculates the logic behind the equations
 function runLogic (symbol, x, y) {
   switch (symbol) {
     case "+":
@@ -134,65 +122,16 @@ function runLogic (symbol, x, y) {
 
 
 
+//when AC button pressed, reset everything
+function reset () {
+  equation = [];
+  temp = '';
+  x = null;
+  y = null;
+  display("");
+  currentAnswer = null;
+}
 
 
 
 
-
-
-
-
-// function capture (evt) {
-
-
-//   switch (evt.target.innerHTML) {
-//     case "AC":
-//       reset();
-//       break;
-//     case "CE":
-//       temp = temp.substring(0,temp.length - 1);
-//       display(temp);
-//       break;
-//     case "=":
-//       addToEquation(temp * sign);
-//       calculate();
-//       break;
-//     case "+":
-//       addToEquation(temp * sign);
-//       addToEquation("+");
-//       temp = '';
-//       display("+");
-//       break;
-//     case "-":
-//       addToEquation(temp * sign);
-//       addToEquation("-");
-//       temp = '';
-//       display("-");
-//       break;
-//     case "X":
-//       addToEquation(temp * sign);
-//       addToEquation("X");
-//       temp = '';
-//       display("X");
-//       break;
-//     case "/":
-//       addToEquation(temp * sign);
-//       addToEquation("/");
-//       temp = '';
-//       display("/");
-//       break;
-//     case "%":
-//       addToEquation(temp * sign);
-//       addToEquation("%");
-//       temp = '';
-//       display("%");
-//       break;
-//     case "+/-":
-//       sign *= -1;
-//       break;
-//     default :
-//       temp += evt.target.innerHTML;
-//       display(temp);
-//   }
-
-// }
